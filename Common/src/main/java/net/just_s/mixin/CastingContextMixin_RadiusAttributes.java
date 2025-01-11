df4e6ac3ecee9f381d47af3daa041e8f54fd0208
Just <grivten@yandex.ru>
2025-01-11 20:50:12 +0300

    easier way to fix the issue with ephemera

--- Common/src/main/java/net/just_s/mixin/CastingContextMixin_RadiusAttributes.java
+++ Common/src/main/java/net/just_s/mixin/CastingContextMixin_RadiusAttributes.java
@@ -1,6 +1,5 @@
 package net.just_s.mixin;
 
-import at.petrak.hexcasting.api.HexAPI;
 import at.petrak.hexcasting.api.spell.casting.CastingContext;
 import at.petrak.hexcasting.xplat.IXplatAbstractions;
 import net.just_s.HexxyAttributesMod;
@@ -8,21 +7,24 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CastingContext.class)
 public class CastingContextMixin_RadiusAttributes {
     @Shadow
     @Final
-    protected ServerPlayerEntity caster;
+    private ServerPlayerEntity caster;
 
-    /**
-     * @author Just_S
-     * @reason due to mixin's inability to modify local variables in this scenario it is easier to overwrite the method.
-     */
-    @Overwrite
-    public final boolean isVecInRange(Vec3d vec) {
+    // Due to Ephemera making a breaking inject into CastingContext (and its overall popularity)
+    // I just made my mixin injecting before Ephemera's does
+    @Inject(
+            method = "isVecInRange",
+            at = @At("HEAD"), cancellable = true, remap = false
+    )
+    public final void hexxyattributes$replaceStaticRadiusesToAttributes(Vec3d vec, CallbackInfoReturnable<Boolean> cir) {
         double sentinel_radius = this.caster.getAttributeValue(HexxyAttributesMod.SENTINEL_RADIUS);
         var sentinel = IXplatAbstractions.INSTANCE.getSentinel(this.caster);
         if (sentinel != null
@@ -30,10 +32,11 @@ public class CastingContextMixin_RadiusAttributes {
                 && this.caster.getWorld().getRegistryKey() == sentinel.dimension()
                 && vec.squaredDistanceTo(sentinel.position()) <= sentinel_radius * sentinel_radius + 0.00000000001
         ) {
-            return true;
+            cir.setReturnValue(true);
+            return;
         }
 
         double ambit_radius = this.caster.getAttributeValue(HexxyAttributesMod.AMBIT_RADIUS);
-        return vec.squaredDistanceTo(this.caster.getPos()) <= ambit_radius * ambit_radius + 0.00000000001;
+        cir.setReturnValue(vec.squaredDistanceTo(this.caster.getPos()) <= ambit_radius * ambit_radius + 0.00000000001);
     }
 }
